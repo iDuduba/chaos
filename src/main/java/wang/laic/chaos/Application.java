33038893315421e5bf0e449a949f374d617b8b54
duduba <ly.deng@gmail.com>
2016-04-12 18:01:23 +0800

    use lombok

--- src/main/java/wang/laic/chaos/Application.java
+++ src/main/java/wang/laic/chaos/Application.java
@@ -2,9 +2,15 @@ package wang.laic.chaos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 @ImportResource (value={"spring-mybatis.xml","shiro-bean.xml" })
 //@ImportResource({
@@ -14,6 +20,16 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class Application {
 
+	@Bean
+	public ApplicationListener<ContextRefreshedEvent> onContextRefeshed() {
+		return new ApplicationListener<ContextRefreshedEvent>() {
+			@Override
+			public void onApplicationEvent(ContextRefreshedEvent event) {
+				log.debug("Initializating application...");
+			}
+		};
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- src/main/java/wang/laic/chaos/web/controller/UserController.java
+++ src/main/java/wang/laic/chaos/web/controller/UserController.java
@@ -2,34 +2,33 @@ package wang.laic.chaos.web.controller;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.github.pagehelper.PageInfo;
 
+import lombok.extern.slf4j.Slf4j;
 import wang.laic.chaos.model.User;
 import wang.laic.chaos.service.UserService;
 
+@Slf4j
 //@RestController
 @Controller
 @RequestMapping("/users")
 public class UserController {
-	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+//	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	
 	@Autowired
     private UserService userService;
 
     @RequestMapping
     public ModelAndView getAll(User user) {
-    	logger.debug("name:{},pass:{},page:{},page.size:{}", user.getUsername(), user.getPassword(), user.getPage(), user.getRows());
+    	log.debug("name:{},pass:{},page:{},page.size:{}", user.getUsername(), user.getPassword(), user.getPage(), user.getRows());
     	
         ModelAndView result = new ModelAndView("userlist");
         List<User> userlist = userService.getAll(user);
